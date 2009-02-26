f7bfd718c2c86fad3f8134d81221db9ff7d5ea33
David Demaree <david@practical.cc>
2009-02-25 21:40:52 -0600

    No home

--- lib/lazy_postman.rb
+++ lib/lazy_postman.rb
@@ -12,10 +12,10 @@ require 'lazy_postman/lazy_postman'
 require 'lazy_postman/email'
 require 'lazy_postman/message'
 
-if File.exists?(config_path = File.expand_path("~/.lazypost/config.rb"))
+if File.exists?(config_path = "/etc/lazypost/config.rb")
   require config_path
 end
 
-if File.exists?(config_path = File.expand_path("~/.lazypost/config.yml"))
+if File.exists?(config_path = "/etc/lazypost/config.yml")
   LazyPostman.load_sites_from_file(config_path)
 end
